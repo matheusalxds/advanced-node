3e598a14f856905b328272bfce3278c2d55ad0af
Matheus Alexandre <mathe.system@gmail.com>
2021-11-27 10:00:25 -0300

    chore: add jest

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Information
 - To create `tsconfig.json` file:
   `npx tsc --init`
-- To compile typescript files
+- To compile typescript files:
   `npx tsc`
-
+- To generate jest config file:
+  `npx jest --init`
 
 > You can verify if Node has support in [NodeGreen](https://node.green).
 
---
+++ jest.config.js
@@ -0,0 +1,32 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'babel',
+
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  moduleNameMapper: {
+    '@/test/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+
+  // A list of paths to directories that Jest should use to search for files in
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
+}
