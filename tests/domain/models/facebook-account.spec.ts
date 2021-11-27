1d7c87b35381cf9afd7b554fa5ec21f3733f2378
Matheus Alexandre <mathe.system@gmail.com>
2021-11-27 19:25:55 -0300

    feat: move business log to a domain entity

---
+++ tests/domain/models/facebook-account.spec.ts
@@ -0,0 +1,37 @@
+import { FacebookAccount } from '@/domain/models'
+
+describe('FacebookAccount', () => {
+  const fbData = {
+    name: 'any_fb_name',
+    email: 'any_fb_email',
+    facebookId: 'any_fb_id'
+  }
+  test('should create with facebook data only', () => {
+    const sut = new FacebookAccount(fbData)
+    expect(sut).toEqual(fbData)
+  })
+
+  test('should update name if its empty', () => {
+    const accountData = { id: 'any_id' }
+    const sut = new FacebookAccount(fbData, accountData)
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email',
+      facebookId: 'any_fb_id'
+    })
+  })
+
+  test('should not update name if its not empty', () => {
+    const accountData = { id: 'any_id', name: 'any_name' }
+    const sut = new FacebookAccount(fbData, accountData)
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_fb_email',
+      facebookId: 'any_fb_id'
+    })
+  })
+})
