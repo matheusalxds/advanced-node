7ab7ee999cde06df3d119fcc69fe3efad21de934
Matheus Alexandre <mathe.system@gmail.com>
2021-11-28 12:07:54 -0300

    feat: ensure FacebookApi gets debug token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -10,12 +10,13 @@ describe('FacebookApi', () => {
   let httpClient: MockProxy<HttpGetClient>
 
   beforeAll(() => {
-    httpClient = mock()
     clientId = 'any_client_id'
     clientSecret = 'any_client_secret'
+    httpClient = mock()
   })
 
   beforeEach(() => {
+    httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -28,6 +29,18 @@ describe('FacebookApi', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+  })
+
+  test('should get debug token', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token'
       }
     })
   })
