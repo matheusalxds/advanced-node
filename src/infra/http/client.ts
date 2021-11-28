d20db05b0bc88269ac1b1c5de33eb83b0c31c670
Matheus Alexandre <mathe.system@gmail.com>
2021-11-28 14:20:06 -0300

    test: ensure AxiosHttpClientAdapter calls axios.get with correct params

--- src/infra/http/client.ts
+++ src/infra/http/client.ts
@@ -2,7 +2,7 @@ export interface HttpGetClient {
   get: <R = any>(params: HttpGetClient.Params) => Promise<R>
 }
 
-namespace HttpGetClient{
+export namespace HttpGetClient {
   export type Params = {
     url: string
     params: object
