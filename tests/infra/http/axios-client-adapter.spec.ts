56bf38ffd0e0d09219871ccc36a0e88ca083559b
Matheus Alexandre <mathe.system@gmail.com>
2021-11-28 14:27:07 -0300

    feat: ensure AxiosHttpClientAdapter returns data on success

--- tests/infra/http/axios-client-adapter.spec.ts
+++ tests/infra/http/axios-client-adapter.spec.ts
@@ -4,8 +4,9 @@ import axios from 'axios'
 jest.mock('axios')
 
 class AxiosHttpClient {
-  async get (args: HttpGetClient.Params): Promise<void> {
-    await axios.get(args.url, { params: args.params })
+  async get <R = any>(args: HttpGetClient.Params): Promise<R> {
+    const result = await axios.get(args.url, { params: args.params })
+    return result.data
   }
 }
 
@@ -19,6 +20,10 @@ describe('AxiosHttpClientAdapter', () => {
     url = 'any_url'
     params = { any: 'any' }
     fakeAxios = axios as jest.Mocked<typeof axios>
+    fakeAxios.get.mockResolvedValue({
+      status: 200,
+      data: 'any_data'
+    })
   })
 
   beforeEach(() => {
@@ -31,6 +36,12 @@ describe('AxiosHttpClientAdapter', () => {
 
       expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
+    })
+
+    test('should return data on success', async () => {
+      const result = await sut.get({ url, params })
+
+      expect(result).toEqual('any_data')
     })
   })
 })
