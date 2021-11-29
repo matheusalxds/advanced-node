beaf4de79c80fd30e793bae509dbbf27b666d84b
Matheus Alexandre <mathe.system@gmail.com>
2021-11-28 22:45:31 -0300

    refactor: isolate SUT creation

--- tests/infra/crypto/jwt-generator-adapter.spec.ts
+++ tests/infra/crypto/jwt-generator-adapter.spec.ts
@@ -15,10 +15,18 @@ export class JwtTokenGeneratorAdapter {
 }
 
 describe('JwtTokenGeneratorAdapter', () => {
-  test('should call sign with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>
-    const sut = new JwtTokenGeneratorAdapter('any_secret')
+  let sut: JwtTokenGeneratorAdapter
+  let fakeJwt: jest.Mocked<typeof jwt>
 
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+  })
+
+  beforeEach(() => {
+    sut = new JwtTokenGeneratorAdapter('any_secret')
+  })
+
+  test('should call sign with correct params', async () => {
     await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
     expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
   })
