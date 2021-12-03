3bcb946e803f0dadbb9b9cf4ea80cbe593a9933c
Matheus Alexandre <mathe.system@gmail.com>
2021-12-02 23:22:43 -0300

    refactor: use genercis instead of any

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,12 +1,20 @@
-import { badRequest, HttpResponse, unauthorized } from '@/application/helpers'
+import { badRequest, HttpResponse, ok, serverError, unauthorized } from '@/application/helpers'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
-import { RequiredFieldError, ServerError } from '@/application/errors'
+import { RequiredFieldError } from '@/application/errors'
+
+type HttpRequest = {
+  token: string | undefined | null
+}
+
+type Model = Error | {
+  accessToken: string
+}
 
 export class FacebookLoginController {
   constructor (private readonly facebookAuthentication: FacebookAuthentication) {}
 
-  async handle (httpRequest: any): Promise<HttpResponse> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     try {
       if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
         return badRequest(new RequiredFieldError('token'))
@@ -14,20 +22,14 @@ export class FacebookLoginController {
 
       const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
       if (accessToken instanceof AccessToken) {
-        return {
-          statusCode: 200,
-          data: {
-            accessToken: accessToken.value
-          }
-        }
+        return ok({
+          accessToken: accessToken.value
+        })
       } else {
         return unauthorized()
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        data: new ServerError(error as Error)
-      }
+      return serverError(error as Error)
     }
   }
 }
--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -1,16 +1,26 @@
-import { UnauthorizedError } from '@/application/errors'
+import { ServerError, UnauthorizedError } from '@/application/errors'
 
-export type HttpResponse = {
+export type HttpResponse<T = any> = {
   statusCode: number
-  data: any
+  data: T
 }
 
-export const badRequest = (error: Error): HttpResponse => ({
+export const serverError = (error: Error): HttpResponse<Error> => ({
+  statusCode: 500,
+  data: new ServerError(error)
+})
+
+export const unauthorized = (): HttpResponse<Error> => ({
+  statusCode: 401,
+  data: new UnauthorizedError()
+})
+
+export const badRequest = (error: Error): HttpResponse<Error> => ({
   statusCode: 400,
   data: error
 })
 
-export const unauthorized = (): HttpResponse => ({
-  statusCode: 401,
-  data: new UnauthorizedError()
+export const ok = <T = any>(data: T): HttpResponse<T> => ({
+  statusCode: 200,
+  data
 })
