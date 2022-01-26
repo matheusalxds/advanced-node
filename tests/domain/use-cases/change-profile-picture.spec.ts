f38fcb2dd4dccc8943b1ed103cf35c661d35d313
Matheus Alexandre <mathe.system@gmail.com>
2022-01-26 16:16:46 -0300

    test: ensure ChangeProfilePicture calls SaveUserPicture with correct input

--- tests/domain/use-cases/change-profile-picture.spec.ts
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -1,5 +1,6 @@
-import { ChangeProfilePicture, setupChangeProfilePicture } from '@/domain/use-cases/change-profile-picture'
+import { ChangeProfilePicture, setupChangeProfilePicture } from '@/domain/use-cases'
 import { UploadFile, UUIDGenerator } from '@/domain/contracts/gateways'
+import { SaveUserPicture } from '@/domain/contracts/repos'
 
 import { mock, MockProxy } from 'jest-mock-extended'
 
@@ -8,18 +9,21 @@ describe('ChangeProfilePicture', () => {
   let file: Buffer
   let fileStorage: MockProxy<UploadFile>
   let crypto: MockProxy<UUIDGenerator>
+  let userProfileRepo: MockProxy<SaveUserPicture>
   let sut: ChangeProfilePicture
 
   beforeEach(() => {
     uuid = 'any_unique_id'
     file = Buffer.from('any_buffer')
-    fileStorage = mock<UploadFile>()
-    crypto = mock<UUIDGenerator>()
+    fileStorage = mock()
+    fileStorage.upload.mockResolvedValue('any_url')
+    crypto = mock()
     crypto.uuid.mockReturnValue(uuid)
+    userProfileRepo = mock()
   })
 
   beforeEach(() => {
-    sut = setupChangeProfilePicture(fileStorage, crypto)
+    sut = setupChangeProfilePicture(fileStorage, crypto, userProfileRepo)
   })
 
   test('should call UploadFile with correct input', async () => {
@@ -29,9 +33,16 @@ describe('ChangeProfilePicture', () => {
     expect(fileStorage.upload).toHaveBeenCalledTimes(1)
   })
 
-  test('should call cal UploadFile when file is undefined', async () => {
+  test('should call UploadFile when file is undefined', async () => {
     await sut({ id: 'any_id', file: undefined })
 
     expect(fileStorage.upload).not.toHaveBeenCalled()
+  })
+
+  test('should call SaveUserPicture with correct input', async () => {
+    await sut({ id: 'any_id', file })
+
+    expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: 'any_url' })
+    expect(userProfileRepo.savePicture).toHaveBeenCalledTimes(1)
   })
 })
