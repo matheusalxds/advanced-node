e698186dd528226dcac573f16afcb5375aa5b600
Matheus Alexandre <mathe.system@gmail.com>
2022-01-27 11:04:07 -0300

    feat: ensure ChangeProfilePicture returns correct data on success

--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -4,16 +4,17 @@ import { UploadFile, UUIDGenerator } from '@/domain/contracts/gateways'
 
 type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator, userProfileRepo: SaveUserPicture & LoadUserProfile) => ChangeProfilePicture
 type Input = { id: string, file?: Buffer }
-export type ChangeProfilePicture = (input: Input) => Promise<void>
+type Output = { pictureUrl?: string, initials?: string }
+export type ChangeProfilePicture = (input: Input) => Promise<Output>
 
 export const setupChangeProfilePicture: Setup = (fileStorage, crpyto, userProfileRepo) => async ({ id, file }) => {
-  const data: {pictureUrl?: string | undefined, name?: string | undefined } = {}
-  if (file !== undefined) {
-    data.pictureUrl = await fileStorage.upload({ file, key: crpyto.uuid({ key: id }) })
-  } else {
-    data.name = (await userProfileRepo.load({ id })).name
+  const key = crpyto.uuid({ key: id })
+  const data = {
+    pictureUrl: file !== undefined ? await fileStorage.upload({ file, key }) : undefined,
+    name: file === undefined ? (await userProfileRepo.load({ id })).name : undefined
   }
   const userProfile = new UserProfile(id)
   userProfile.setPicture(data)
   await userProfileRepo.savePicture(userProfile)
+  return userProfile
 }
