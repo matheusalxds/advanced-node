13d2ebf0c7c2a288d95dd528e6792418755cba4c
Matheus <mathe.system@gmail.com>
2022-02-21 15:06:10 -0300

    feat: add Validators to builder

--- src/application/controllers/save-picture.ts
+++ src/application/controllers/save-picture.ts
@@ -1,7 +1,7 @@
 import { ChangeProfilePicture } from '@/domain/use-cases'
 import { HttpResponse, ok } from '@/application/helpers'
 import { Controller } from '@/application/controllers/controller'
-import { AllowedMimeTypes, MaxFileSize, Required, RequiredBuffer, Validator } from '@/application/validation'
+import { ValidationBuilder as Builder, Validator } from '@/application/validation'
 
 type HttpRequest = { file: { buffer: Buffer, mimeType: string }, userId: string }
 type Model = Error | { initials?: string, pictureUrl?: string}
@@ -13,16 +13,15 @@ export class SavePictureController extends Controller {
 
   override async perform ({ file, userId }: HttpRequest): Promise<HttpResponse<Model>> {
     const data = await this.changeProfilePicture({ id: userId, file: file.buffer })
-
     return ok(data)
   }
 
   override buildValidators ({ file }: HttpRequest): Validator[] {
     return [
-      new Required(file, 'file'),
-      new RequiredBuffer(file.buffer, 'file'),
-      new AllowedMimeTypes(['png', 'jpg'], file.mimeType),
-      new MaxFileSize(5, file.buffer)
+      ...Builder.of({ value: file, fieldName: 'file' })
+        .required()
+        .image({ allowed: ['png', 'jpg'], maxSizeInMb: 5 })
+        .build()
     ]
   }
 }
--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,9 +1,87 @@
-import { RequiredString, ValidationBuilder } from '@/application/validation'
+import {
+  AllowedMimeTypes,
+  MaxFileSize,
+  Required,
+  RequiredBuffer,
+  RequiredString,
+  ValidationBuilder
+} from '@/application/validation'
 
 describe('ValidationBuilder', () => {
-  test('should return a RequiredString', () => {
-    const validators = ValidationBuilder.of({ value: 'any_value', fieldName: 'any_name' }).required().build()
+  test('should return RequiredString', () => {
+    const validators = ValidationBuilder
+      .of({ value: 'any_value' })
+      .required()
+      .build()
 
-    expect(validators).toEqual([new RequiredString('any_value', 'any_name')])
+    expect(validators).toEqual([new RequiredString('any_value')])
+  })
+
+  test('should return RequiredBuffer', () => {
+    const buffer = Buffer.from('any_buffer')
+
+    const validators = ValidationBuilder
+      .of({ value: buffer })
+      .required()
+      .build()
+
+    expect(validators).toEqual([new RequiredBuffer(buffer)])
+  })
+
+  test('should return Required', () => {
+    const validators = ValidationBuilder
+      .of({ value: { any: 'any' } })
+      .required()
+      .build()
+
+    expect(validators).toEqual([new Required({ any: 'any' })])
+  })
+
+  test('should return Required', () => {
+    const buffer = Buffer.from('any_buffer')
+
+    const validators = ValidationBuilder
+      .of({ value: { buffer } })
+      .required()
+      .build()
+
+    expect(validators).toEqual([
+      new Required({ buffer }),
+      new RequiredBuffer(buffer)
+    ])
+  })
+
+  test('should return correct image validators', () => {
+    const buffer = Buffer.from('any_buffer')
+
+    const validators = ValidationBuilder
+      .of({ value: { buffer } })
+      .image({ allowed: ['png'], maxSizeInMb: 6 })
+      .build()
+
+    expect(validators).toEqual([new MaxFileSize(6, buffer)])
+  })
+
+  test('should return correct image validators', () => {
+    const validators = ValidationBuilder
+      .of({ value: { mimeType: 'image/png' } })
+      .image({ allowed: ['png'], maxSizeInMb: 6 })
+      .build()
+
+    expect(validators).toEqual([new AllowedMimeTypes(['png'], 'image/png')])
+  })
+
+  test('should return correct image validators', () => {
+    const buffer = Buffer.from('any_buffer')
+
+    const validators = ValidationBuilder
+      .of({ value: { buffer, mimeType: 'image/png' } })
+      .image({ allowed: ['png'], maxSizeInMb: 6 })
+      .build()
+
+    expect(validators).toEqual([
+      new AllowedMimeTypes(['png'], 'image/png'),
+      new MaxFileSize(6, buffer)
+    ])
   })
 })
